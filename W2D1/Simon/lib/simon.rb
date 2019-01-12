6bc7db44abfee4d91a59c96fe4d84f2625e03271
darrenyong <contact.darrenyong@gmail.com>
2019-01-11 19:18:30 -0800

    Added #initialize method for Simon class

--- W2D1/Simon/lib/simon.rb
+++ W2D1/Simon/lib/simon.rb
@@ -4,19 +4,31 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
+    until @game_over
+      take_turn
+    end
 
+    if @game_over
+      game_over_message
+      reset_game
+    end
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    round_success_message
+    @sequence_length += 1
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,18 +36,22 @@ class Simon
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    @game_over = true
+    puts "Congratulations! You won :)"
   end
 
   def game_over_message
-
+    @game_over = true
+    puts "Game over! You lost :("
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
