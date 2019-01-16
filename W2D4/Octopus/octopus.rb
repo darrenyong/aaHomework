db461c21ebc33825caa932e91acd00cf98e53fdf
darrenyong <contact.darrenyong@gmail.com>
2019-01-16 13:22:47 -0800

    Added sorting algorithm to find longest fish in O(n log n) time

--- W2D4/Octopus/octopus.rb
+++ W2D4/Octopus/octopus.rb
@@ -26,6 +26,32 @@ end
 # Dominant Octopus - O(n log n)
 # dominant_octopus(fishes)
 def dominant_octopus(arr)
-  
+  return arr if arr.length <= 1
 
+  mid = arr.length/2
+  left_half = dominant_octopus(arr[0...mid])
+  right_half = dominant_octopus(arr[mid..-1])
+
+  sorted_oct_arr = merge_oct(left_half, right_half)
 end
+
+def merge_oct(left, right)
+  merged_arr = []
+
+  until left.empty? || right.empty?
+    left_length = left[0].length
+    right_length = right[0].length
+    if left_length < right_length
+      merged_arr << left.shift
+    elsif right_length < left_length
+      merged_arr << right.shift
+    elsif left_length == right_length
+      merged_arr << left.shift
+    end
+  end
+
+  merged_arr + left + right
+end
+
+sorted_oct_arr = dominant_octopus(fishes)
+sorted_oct_arr.last
