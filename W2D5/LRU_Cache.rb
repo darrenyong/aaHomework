08502333c7fc59d61eda7f8999fbefed60d91864
darrenyong <contact.darrenyong@gmail.com>
2019-01-17 19:49:28 -0800

    Added test cases for LRU Cache

--- W2D5/LRU_Cache.rb
+++ W2D5/LRU_Cache.rb
@@ -1,9 +1,11 @@
  class LRUCache
-    def initialize
+    def initialize(size)
+      @cache = Array.new(size)
     end
 
     def count
       # returns number of elements currently in cache
+      @cache.length
     end
 
     def add(el)
@@ -18,3 +20,25 @@
     # helper methods go here!
 
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  johnny_cache = LRUCache.new(4)
+
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(5)
+
+  johnny_cache.count # => returns 2
+
+  johnny_cache.add([1,2,3])
+  johnny_cache.add(5)
+  johnny_cache.add(-5)
+  johnny_cache.add({a: 1, b: 2, c: 3})
+  johnny_cache.add([1,2,3,4])
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(:ring_of_fire)
+  johnny_cache.add("I walk the line")
+  johnny_cache.add({a: 1, b: 2, c: 3})
+
+  p johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
+end
