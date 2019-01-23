f7a31d9a158a4421c93069bd28b6d59644985ab1
darrenyong <contact.darrenyong@gmail.com>
2019-01-22 21:34:27 -0800

    Ran migration for house table

--- W3D2/demo_project/db/migrate/20190123043308_create_house_table.rb
+++ W3D2/demo_project/db/migrate/20190123043308_create_house_table.rb
@@ -1,6 +1,9 @@
 class CreateHouseTable < ActiveRecord::Migration[5.2]
   def change
     create_table :house_tables do |t|
+      t.string :address, null: false
+      
+      t.timestamps
     end
   end
 end
