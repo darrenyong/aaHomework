df4288a2cce5a203ab1a9af2e4833000408938f2
darrenyong <contact.darrenyong@gmail.com>
2019-01-30 08:32:19 -0800

    Added methods to reset and ensure user session tokens

--- W4D2/rails_auth/app/models/user.rb
+++ W4D2/rails_auth/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6, allow_nil: true }
   before_validation :ensure_session_token
 
-
+  attr_reader :password
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
@@ -14,5 +14,17 @@ class User < ApplicationRecord
   def self.generate_session_token
     SecureRandom::urlsafe_base64(16)
   end
-  
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    generate_session_token unless self.session_token
+  end
+
+  def password=(value)
+    @password = value
+  end
+
 end
