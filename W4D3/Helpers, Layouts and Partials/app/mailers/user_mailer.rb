66a607dbf9ec3b316585f72d927c7e215146f27f
darrenyong <contact.darrenyong@gmail.com>
2019-01-30 19:39:20 -0800

    Created a mailing template for a welcome email

--- W4D3/Helpers, Layouts and Partials/app/mailers/user_mailer.rb
+++ W4D3/Helpers, Layouts and Partials/app/mailers/user_mailer.rb
@@ -1,6 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: "everybody@appacademy.io"
 
-
+  def welcome_email(user)
+    @user = user
+    @url  = 'https://www.appacademy.io/'
+    mail(to: user.email, subject: 'Welcome to App Academy')
+  end
 
 end
