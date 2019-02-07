97c9b19715b904d3543476f4d67138942fba2c9e
darrenyong <contact.darrenyong@gmail.com>
2019-02-06 20:01:52 -0800

    Initial commit for W5D3 Intro to JavaScript homework

---
+++ W5D3/introToJavaScript.js
@@ -0,0 +1,10 @@
+// madLib
+function madLib(verb, adjective, noun) {
+  var capVerb = verb.toUpperCase()
+  var capAdjective = adjective.toUpperCase()
+  var capNoun = noun.toUpperCase()
+
+  console.log(`we shall ${capVerb} the ${capAdjective} ${capNoun}`)
+}
+
+// isSubstring
