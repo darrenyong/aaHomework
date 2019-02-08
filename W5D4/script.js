0daa76659f33c1996a00ed2316275f7da95a17a2
darrenyong <contact.darrenyong@gmail.com>
2019-02-07 22:10:57 -0800

    Added simpleTimeout and hammerTime that makes a callback to simpleTimeout

--- W5D4/script.js
+++
@@ -1,6 +0,0 @@
-// Simple Timeout
-const simpleTimeout = () => window.setTimeout( () => alert('HAMMERTIME'), 5000)
-
-
-// Timeout Plus Closure
-const hammerTime = (time) => window.setTimeout( simpleTimeout() )
