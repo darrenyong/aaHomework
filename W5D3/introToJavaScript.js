58d33d97315f8900c486b3b80bcd88bfaf19c01c
darrenyong <contact.darrenyong@gmail.com>
2019-02-07 22:09:05 -0800

    Added fizzbuzz method

--- W5D3/introToJavaScript.js
+++ W5D3/introToJavaScript.js
@@ -5,9 +5,16 @@ function madLib(verb, adjective, noun) {
   var capNoun = noun.toUpperCase()
 
   console.log(`we shall ${capVerb} the ${capAdjective} ${capNoun}`);
-}
+};
 
 // isSubstring
 function isSubstring(searchString, subString) {
   console.log(searchString.includes(subString));
+};
+
+// fizzBuzz
+function fizzBuzz(array) {
+  for (var i = 0; i < array.length; i++) {
+    
+  }
 }
