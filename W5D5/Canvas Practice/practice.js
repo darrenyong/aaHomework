1c3e59c184649c6a40930ea7a6abad953619e120
darrenyong <contact.darrenyong@gmail.com>
2019-02-11 08:24:54 -0800

    Filled ctx rectangle red

--- W5D5/Canvas Practice/practice.js
+++ W5D5/Canvas Practice/practice.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
   const canvasElement = document.getElementById("mycanvas");
-  canvasElement.width = 500px;
-  canvasElement.height = 500px;
+  canvasElement.width = 500;
+  canvasElement.height = 500;
 
   const ctx = canvasElement.getContext("2d");
-
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, 500, 500);
 
 });
