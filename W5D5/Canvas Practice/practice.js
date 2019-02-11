bc508bf9955ad8e13b4176170f2b3448dea91def
darrenyong <contact.darrenyong@gmail.com>
2019-02-11 08:22:37 -0800

    Grabbed the canvas element and set height and width to 500px

--- W5D5/Canvas Practice/practice.js
+++ W5D5/Canvas Practice/practice.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
+  const canvasElement = document.getElementById("mycanvas");
+  canvasElement.width = 500px;
+  canvasElement.height = 500px;
+
+
 
 });
